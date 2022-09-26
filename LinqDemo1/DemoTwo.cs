8c3cb315c567c0ab7b79cc70942b7180401b21ad
Sachinghorpade97 <ghorpadesachin382@gmail.com>
2022-09-26 09:22:39 +0530

    Add project files.

---
+++ LinqDemo1/DemoTwo.cs
@@ -0,0 +1,28 @@
+﻿
+class DemoTwo
+{
+    public static void LinqDemo2()
+    {
+        string sentence = "wellcome";
+        string[] catName = {"W","E","L","L","C","O","M","E"};
+        int[] numbers = { 3, 1, 2, 6, 3, 8, 9, 4, 7, 5};
+
+        var getTheNumbers = from number in numbers
+                            where number < 5
+                            select number;
+
+        List<int> newNumbers = new List<int>();
+
+        foreach (var number in numbers)
+        {
+            if (number < 5)
+            {
+                newNumbers.Add(number);
+            }
+        }
+
+        System.Console.WriteLine(string.Join(", ", getTheNumbers));
+        System.Console.WriteLine(string.Join(", ", newNumbers));
+    }
+
+}
---
+++ LinqDemo1/program1.cs
@@ -0,0 +1,15 @@
+﻿
+class program
+{
+    public static void main()
+    {
+        int[] age = { 12, 23, 21, 22, 45, 33, 20, 26, 55, 66 };
+        var a = from i in age where i > 20 orderby i descending select i;
+
+        foreach (int item in a)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadLine();
+    }
+}
